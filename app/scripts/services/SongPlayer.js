3682d34dfa7385c1cfedafb3f2cf527bd4de615b
Kara <kara.a.flaherty@gmail.com>
2017-04-15 01:58:13 -0600

    Assignment 7: Services: Part 2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,50 +1,74 @@
 (function() {
     function SongPlayer() {
         var SongPlayer = {};
+
+        /**
+         * @desc The most recently chosen song
+         * @type {Object}
+         */
         var currentSong = null;
-        
+
         /**
-        * @desc Buzz object audio file
-        * @type {Object}
-        */
+         * @desc Buzz object audio file
+         * @type {Object}
+         */
         var currentBuzzObject = null;
-        
+
         /**
-        * @function setSong
-        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-        * @param {Object} song
-        */
+         * @function setSong
+         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+         * @param {Object} song
+         */
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
                 currentSong.playing = null;
             }
-            
+
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
-            
+
             currentSong = song;
         };
-        
+
+        /**
+         * @function playSong
+         * @desc Plays the audio file of currentBuzzObject and changes the song's status to playing
+         * @param {Object} song
+         */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+
+        /**
+         * @function SongPlayer.play
+         * @desc Sets up the newly chosen song to play or plays the currently paused audio file
+         * @param {Object} song
+         */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
-        
+
+        /**
+         * @function SongPlayer.pause
+         * @desc Pauses the current audio file and changes the song's status to paused
+         * @param {Object} song
+         */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
         };
-        
+
         return SongPlayer;
     }
 
